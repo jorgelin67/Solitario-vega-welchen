bc179246a86e3d9215da1f6dbcc6090321d86214
jorgelin67 <jorge.vega@sagradoalcal.edu.ar>
2024-05-16 15:08:55 -0300

    click en cartas

--- index.js
+++ index.js
@@ -1,40 +1,72 @@
 class EscenaInicial extends Phaser.Scene{
 
     mazo = []
-
-preload(){
-    this.load.spritesheet("baraja","assets/sprites/baraja_2.png",{frameWidth:104,frameHeight:160 })
-}
-
-
+    intentos = 4
+    
     matriz_solitario = []
     selecciono = true
     titulo=undefined
-    mazo = Array()
+
+
+preload(){
+    this.load.spritesheet("baraja","assets/sprites/baraja_2.png",{frameWidth:104,frameHeight:160 })
+    
+}
+
+
 
 
     cargar_texto(){
+        this.add.text(650,90, "mis cartas",{
+           Fill : 'purple',
+           fontSize: 60
+    
 
-   
+
+        });
+    this.add.text(100,500, "mis intentos:"+ this.intentos, {
+        fontSize: 22
+
+        })
     }
 
+    clickCarta(carta){
+        console.log(carta)
+    }
 
 
     cargar_matriz_inicial(){
         let posicion_x = 50
         let posicion_y = 160
-        for (let fila = 0; fila < 4; fila++) {
-            for (let columna = 0; columna < 10; columna++) {
-                console.log("fila:",fila,"columna:",columna)
-                this.add.sprite(posicion_x, posicion_y, "baraja").setFrame(49)
-                posicion_x+=104 + 10
-            }
+        let fila = 0
+        let columna = 0
+        for (let j = 0,length = 36; j < length; j++ ){
+            //columna+=1
+            let cartasJson = {"palo": this.mazo[j].palo,
+            "fila": posicion_y, "columna": posicion_x, "valor": this.mazo[j].columna,
+        "frame":this.mazo[j].frame}
+        this.add.sprite(posicion_x,posicion_y, "baraja").setScale(0.5)
+        .setFrame(49)
+        .setInteractive()
+        .on("pointerdown",() =>{
+            this.clickCarta(cartasJson);
+        } )
+        if(columna == 8){
+            columna = 0 
             posicion_x = 50
-            posicion_y+=160
+            posicion_y += 80
+
+        }else{
+            posicion_x += 52
+            columna += 1
         }
-
-
+        }
+    
     }
+
+ 
+
+ 
 
  asignarPalo(fila) {
     let palo = "";
@@ -77,13 +109,19 @@ preload(){
 }//fin funcion
 
     barajar_mazo(){
-       
+       return this.mazo.sort(()=>Math.random() - 0.5);
     }
 
+
+
     mostrar_mis_cartas(){
-        let pos_x = 80
-        let pos_y = 600
+        let pos_x = 710
+        let pos_y = 190
         
+        for (let i = 39; i>=36; i--) {
+            this.add.sprite(pos_x, pos_y, "baraja").setFrame(this.mazo[i].frame).setScale(0.5)
+            pos_y += 80
+        }
     }
 
 
@@ -93,9 +131,7 @@ preload(){
     //Por ejemplo, haremos acciones como ubicar 
     //las imagénes en la pantalla
     create(){
-        //carga la matriz principal
-        this.cargar_matriz_inicial()
-
+     
         //muestra el texto Mis Cartas
         this.cargar_texto()
 
@@ -104,6 +140,10 @@ preload(){
 
         //desordenar aleatoriamente
         this.barajar_mazo()
+
+           //carga la matriz principal
+           this.cargar_matriz_inicial()
+
         //muestra las 4 cartas para intentar resolver el solitario
         this.mostrar_mis_cartas()
     }
